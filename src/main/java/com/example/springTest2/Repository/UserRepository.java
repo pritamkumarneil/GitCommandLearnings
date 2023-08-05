e8246ecd7d8912e683976593b2449354e0816484
Pritam Kumar <neilpritamk@gmail.com>
2023-08-05 15:10:23 +0530

    made changes in users

--- src/main/java/com/example/springTest2/Repository/UserRepository.java
+++ src/main/java/com/example/springTest2/Repository/UserRepository.java
@@ -2,8 +2,13 @@ package com.example.springTest2.Repository;
 
 import com.example.springTest2.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
+    @Query(value = "select * from user",nativeQuery = true)
+    public List<User> findAll();
 }
